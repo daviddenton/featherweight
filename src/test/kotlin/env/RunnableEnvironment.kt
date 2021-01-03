d772c0f77abb4e5d2a1a970c68f23d87ce6015e1
David Denton <denton.david@gmail.com>
2021-01-03 00:01:59 +0000

    adding stuff to runnable environment

--- src/test/kotlin/env/RunnableEnvironment.kt
+++ src/test/kotlin/env/RunnableEnvironment.kt
@@ -1,28 +1,49 @@
 package env
 
+import dev.forkhandles.result4k.get
 import featherweight.FeatherweightApp
 import org.http4k.client.JavaHttpClient
 import org.http4k.cloudnative.env.Environment
 import org.http4k.connect.amazon.kms.FakeKMS
+import org.http4k.connect.amazon.kms.createKey
 import org.http4k.connect.amazon.lambda.FakeLambda
+import org.http4k.connect.amazon.model.BucketName
+import org.http4k.connect.amazon.model.CustomerMasterKeySpec.SYMMETRIC_DEFAULT
+import org.http4k.connect.amazon.model.ParameterType
+import org.http4k.connect.amazon.model.Region
 import org.http4k.connect.amazon.s3.FakeS3
+import org.http4k.connect.amazon.s3.createBucket
 import org.http4k.connect.amazon.secretsmanager.FakeSecretsManager
+import org.http4k.connect.amazon.secretsmanager.createSecret
 import org.http4k.connect.amazon.systemsmanager.FakeSystemsManager
+import org.http4k.connect.amazon.systemsmanager.putParameter
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import java.time.Clock
+import java.util.UUID
 
 fun main() {
     val clock = Clock.systemDefaultZone()
 
-    FakeS3(clock = clock).start()
-    FakeKMS(clock = clock).start()
+    val region = Region.of("ldn-north-1")
+
+    FakeS3(clock = clock).apply {
+        s3Client().createBucket(BucketName.of("mybucket"), region)
+    }.start()
+
+    FakeKMS(clock = clock).apply {
+        println(client().createKey(SYMMETRIC_DEFAULT).get())
+    }.start()
     FakeLambda(clock = clock).start()
-    FakeSecretsManager(clock = clock).start()
-    FakeSystemsManager(clock = clock).start()
+    FakeSecretsManager(clock = clock).apply {
+        println(client().createSecret("mysecret", UUID.randomUUID(), "secretValue").get())
+    }.start()
+    FakeSystemsManager(clock = clock).apply {
+        println(client().putParameter("myParameter", "myParamValue", ParameterType.String).get())
+    }.start()
 
     val env = Environment.from(
-        "AWS_REGION" to "ldn-north-1",
+        "AWS_REGION" to region.value,
         "AWS_ACCESS_KEY_ID" to "accessKeyId",
         "AWS_SECRET_ACCESS_KEY" to "secretAccessKey"
     )
