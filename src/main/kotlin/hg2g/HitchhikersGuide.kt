d1b490b763e34511ef4b821dc1a5020a22b4480e
David Denton <denton.david@gmail.com>
2021-01-08 21:40:30 +0000

    upgrades

--- src/main/kotlin/hg2g/HitchhikersGuide.kt
+++ src/main/kotlin/hg2g/HitchhikersGuide.kt
@@ -4,12 +4,18 @@ import hg2g.Settings.API_KEY_SECRET_ID
 import hg2g.Settings.AWS_ACCOUNT
 import hg2g.Settings.AWS_CREDENTIALS
 import hg2g.Settings.AWS_REGION
+import hg2g.Settings.KMS_URL
+import hg2g.Settings.LAMBDA_URL
+import hg2g.Settings.SECRETS_MANAGER_URL
 import hg2g.Settings.SIGNING_KEY_ID_PARAMETER
+import hg2g.Settings.SQS_URL
+import hg2g.Settings.SSM_URL
 import hg2g.Settings.SUBMISSION_QUEUE
 import hg2g.Settings.TRANSLATOR_LAMBDA
 import org.http4k.aws.AwsCredentials
 import org.http4k.cloudnative.env.Environment
 import org.http4k.cloudnative.env.EnvironmentKey
+import org.http4k.connect.amazon.AwsServiceCompanion
 import org.http4k.connect.amazon.kms.Http
 import org.http4k.connect.amazon.kms.KMS
 import org.http4k.connect.amazon.lambda.Http
@@ -27,7 +33,9 @@ import org.http4k.connect.amazon.systemsmanager.Http
 import org.http4k.connect.amazon.systemsmanager.SystemsManager
 import org.http4k.core.HttpHandler
 import org.http4k.core.then
+import org.http4k.filter.ClientFilters.SetHostFrom
 import org.http4k.lens.composite
+import org.http4k.lens.uri
 import org.http4k.routing.routes
 import java.time.Clock
 
@@ -44,25 +52,32 @@ object Settings {
     val TRANSLATOR_LAMBDA = EnvironmentKey.map(FunctionName::of).required("TRANSLATOR_LAMBDA")
     val AWS_ACCOUNT = EnvironmentKey.map(AwsAccount::of).required("AWS_ACCOUNT")
     val SUBMISSION_QUEUE = EnvironmentKey.map(QueueName::of).required("SUBMISSION_QUEUE")
+    val SQS_URL = EnvironmentKey.uri().defaulted("SQS_URL", SQS.awsUrl())
+    val LAMBDA_URL = EnvironmentKey.uri().defaulted("LAMBDA_URL", Lambda.awsUrl())
+    val SECRETS_MANAGER_URL = EnvironmentKey.uri().defaulted("SECRETS_MANAGER_URL", SecretsManager.awsUrl())
+    val KMS_URL = EnvironmentKey.uri().defaulted("KMS_URL", KMS.awsUrl())
+    val SSM_URL = EnvironmentKey.uri().defaulted("SSM_URL", SystemsManager.awsUrl())
+
+    private fun AwsServiceCompanion.awsUrl() = EnvironmentKey.composite { awsService.toUri(AWS_REGION(it)) }
 }
 
 /**
  * Our main HTTP API.
  */
 fun HitchhikersGuideApp(env: Environment, http: HttpHandler, clock: Clock): HttpHandler {
-    val kms = KMS.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, http, clock)
-    val systemsManager = SystemsManager.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, http, clock)
-    val secretsManager = SecretsManager.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, http, clock)
-    val sqs = SQS.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, http, clock)
-    val lambda = Lambda.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, http, clock)
+    val kms = KMS.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, SetHostFrom(KMS_URL(env)).then(http), clock)
+    val ssm = SystemsManager.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, SetHostFrom(SSM_URL(env)).then(http), clock)
+    val sm = SecretsManager.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, SetHostFrom(SECRETS_MANAGER_URL(env)).then(http), clock )
+    val sqs = SQS.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, SetHostFrom(SQS_URL(env)).then(http), clock)
+    val lambda = Lambda.Http(AWS_REGION(env), { AWS_CREDENTIALS(env) }, SetHostFrom(LAMBDA_URL(env)).then(http), clock)
 
-    return ApiKeySecurity(secretsManager, API_KEY_SECRET_ID(env))
+    return ApiKeySecurity(sm, API_KEY_SECRET_ID(env))
         .then(
             routes(
                 SubmitArticle(
                     LambdaBabelFish(lambda, TRANSLATOR_LAMBDA(env)),
                     SigningSQSEditor(
-                        KmsSigner(kms, systemsManager, SIGNING_KEY_ID_PARAMETER(env)),
+                        KmsSigner(kms, ssm, SIGNING_KEY_ID_PARAMETER(env)),
                         sqs, AWS_ACCOUNT(env), SUBMISSION_QUEUE(env)))
             )
         )
--- src/test/kotlin/env/RunnableEnvironment.kt
+++ src/test/kotlin/env/RunnableEnvironment.kt
@@ -1,22 +1,36 @@
 package env
 
-import dev.forkhandles.result4k.get
+import dev.forkhandles.result4k.valueOrNull
 import hg2g.HitchhikersGuideApp
-import org.http4k.client.JavaHttpClient
 import org.http4k.cloudnative.env.Environment
+import org.http4k.connect.amazon.AwsServiceCompanion
 import org.http4k.connect.amazon.kms.FakeKMS
+import org.http4k.connect.amazon.kms.KMS
 import org.http4k.connect.amazon.kms.createKey
 import org.http4k.connect.amazon.lambda.FakeLambda
-import org.http4k.connect.amazon.model.BucketName
+import org.http4k.connect.amazon.lambda.Lambda
+import org.http4k.connect.amazon.model.AwsAccount
 import org.http4k.connect.amazon.model.CustomerMasterKeySpec.SYMMETRIC_DEFAULT
+import org.http4k.connect.amazon.model.FunctionName
 import org.http4k.connect.amazon.model.ParameterType
+import org.http4k.connect.amazon.model.QueueName
 import org.http4k.connect.amazon.model.Region
-import org.http4k.connect.amazon.s3.FakeS3
-import org.http4k.connect.amazon.s3.createBucket
 import org.http4k.connect.amazon.secretsmanager.FakeSecretsManager
+import org.http4k.connect.amazon.secretsmanager.SecretsManager
 import org.http4k.connect.amazon.secretsmanager.createSecret
+import org.http4k.connect.amazon.sqs.FakeSQS
+import org.http4k.connect.amazon.sqs.SQS
+import org.http4k.connect.amazon.sqs.createQueue
 import org.http4k.connect.amazon.systemsmanager.FakeSystemsManager
+import org.http4k.connect.amazon.systemsmanager.SystemsManager
 import org.http4k.connect.amazon.systemsmanager.putParameter
+import org.http4k.core.HttpHandler
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.routing.bind
+import org.http4k.routing.header
+import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import java.time.Clock
@@ -27,25 +41,46 @@ fun main() {
 
     val region = Region.of("ldn-north-1")
 
-    FakeS3(clock = clock).apply {
-        s3Client().createBucket(BucketName.of("mybucket"), region)
-    }.start()
+    val queueName = QueueName.of("editorialQueue")
+    val awsAccount = AwsAccount.of("1234567890")
+    val secretName = "mysecret"
+    val lambdaName = FunctionName("babelfish")
 
-    FakeKMS(clock = clock).apply {
-        println(client().createKey(SYMMETRIC_DEFAULT).get())
-    }.start()
-    FakeLambda(clock = clock).start()
-    FakeSecretsManager(clock = clock).apply {
-        println(client().createSecret("mysecret", UUID.randomUUID(), "secretValue").get())
-    }.start()
-    FakeSystemsManager(clock = clock).apply {
-        println(client().putParameter("myParameter", "myParamValue", ParameterType.String).get())
-    }.start()
+    val sqs = FakeSQS(clock = clock, awsAccount = awsAccount)
+    val kms = FakeKMS(clock = clock)
+    val lambda = FakeLambda(lambdaName to { _: Request -> Response(Status.OK) }, clock = clock)
+    val systemsManager = FakeSystemsManager(clock = clock)
+    val secretsManager = FakeSecretsManager(clock = clock)
+
+    // setup infra
+    sqs.client().createQueue(queueName, emptyMap(), emptyMap())
+    val keyId = kms.client().createKey(SYMMETRIC_DEFAULT).valueOrNull()!!.KeyMetadata.KeyId
+    secretsManager.client().createSecret(secretName, UUID.randomUUID(), "secretValue")
+    systemsManager.client().putParameter("myParameter", "myParamValue", ParameterType.String)
 
     val env = Environment.from(
         "AWS_REGION" to region.value,
         "AWS_ACCESS_KEY_ID" to "accessKeyId",
-        "AWS_SECRET_ACCESS_KEY" to "secretAccessKey"
+        "AWS_SECRET_ACCESS_KEY" to "secretAccessKey",
+        "SUBMISSION_QUEUE" to queueName.value,
+        "AWS_ACCOUNT" to awsAccount.value,
+        "API_KEY_SECRET_ID" to keyId.value,
+        "SIGNING_KEY_ID_PARAMETER" to secretName,
+        "TRANSLATOR_LAMBDA" to lambdaName.value,
     )
-    HitchhikersGuideApp(env, JavaHttpClient(), clock).asServer(SunHttp(8080)).start()
+
+    val http = Hosts(
+        KMS to kms,
+        SecretsManager to secretsManager,
+        SystemsManager to systemsManager,
+        Lambda to lambda,
+        SQS to sqs
+    )
+    HitchhikersGuideApp(env, http, clock).asServer(SunHttp(8080))
 }
+
+fun Hosts(vararg services: Pair<AwsServiceCompanion, HttpHandler>) = routes(
+    *services.map { service ->
+        header("host") { it.contains(service.first.awsService.value) } bind service.second
+    }.toTypedArray()
+)
